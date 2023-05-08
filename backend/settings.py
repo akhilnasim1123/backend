f8587ef377411a0c2062a0693074397149db1cf0
akhil <akhilnasim029@gmail.com>
2023-05-08 19:41:19 +0530

    nothing

--- backend/settings.py
+++ backend/settings.py
@@ -22,7 +22,6 @@ load_dotenv()
 env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-environ.Env.read_env(os.path.join(BASE_DIR,'.env'))
 
 
 # Quick-start development settings - unsuitable for production
